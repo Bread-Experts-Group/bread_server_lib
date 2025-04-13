1835076d026c506480af3a8605a65844e0d64692
ATPStorages <tcgytsc@gmail.com>
2025-04-13 10:05:52 -0500

    HTTPS binding enumerations

---
+++ src/main/kotlin/dns/https/HTTPSParameters.kt
@@ -0,0 +1,15 @@
+package bread_experts_group.dns.https
+
+enum class HTTPSParameters(val code: Int) {
+	MANDATORY(0),
+	ADDITIONAL_SUPPORTED_PROTOCOLS(1),
+	NO_SUPPORT_FOR_DEFAULT_PROTOCOL(2),
+	ALTERNATIVE_PORT(3),
+	IPV4_HINT(4),
+	ENCRYPTED_CLIENT_HELLO(5),
+	IPV6_HINT(6);
+
+	companion object {
+		val mapping = entries.associateBy(HTTPSParameters::code)
+	}
+}
