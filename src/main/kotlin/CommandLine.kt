0e7f8f172a4c2b07d59d6ef6b0751f768df8a433
ATPStorages <tcgytsc@gmail.com>
2025-04-14 15:51:38 -0500

    Argument logging

--- src/main/kotlin/CommandLine.kt
+++ src/main/kotlin/CommandLine.kt
@@ -1,5 +1,7 @@
 package bread_experts_group
 
+import java.util.logging.Logger
+
 fun stringToLong(str: String): Long =
 	if (str.substring(0, 1) == "0x") str.substring(2).toLong(16)
 	else if (str.substring(0, 1) == "0b") str.substring(2).toLong(2)
@@ -29,6 +31,7 @@ fun readArgs(
 	vararg flags: Flag<*>
 ) = readArgs(args, flags.toList())
 
+private val logger = Logger.getLogger("Bread Server Library Argument Retrieval")
 fun readArgs(
 	args: Array<String>,
 	flags: List<Flag<*>>
@@ -36,11 +39,15 @@ fun readArgs(
 	val singleArgs = mutableMapOf<String, Any>()
 	val multipleArgs = mutableMapOf<String, MutableList<Any>>()
 	args.forEach {
+		logger.finer { "Parse argument \"$it\"" }
 		if (it[0] != '-') throw IllegalArgumentException("Bad argument \"$it\", requires - before name")
 		var equIndex = it.indexOf('=')
 		val flag = flags.first { f -> f.flagName == it.substring(1, if (equIndex == -1) it.length else equIndex) }
 		val value = if (equIndex == -1) "true" else it.substring(equIndex + 1)
 		val typedValue = if (value.isNotBlank()) flag.conv(value) else flag.default
+		logger.finer {
+			"Conversion \"${typedValue.toString()}\" ${if (typedValue != null) "(${typedValue::class.simpleName})" else ""}"
+		}
 		if (typedValue != null) {
 			if (flag.repeatable) {
 				multipleArgs
@@ -53,8 +60,10 @@ fun readArgs(
 		}
 	}
 	flags.forEach {
-		if (!it.repeatable && it.default != null && !singleArgs.contains(it.flagName))
+		if (!it.repeatable && it.default != null && !singleArgs.contains(it.flagName)) {
+			logger.finer { "Using default (\"${it.default}\" (${it.default::class.simpleName})) for flag \"${it.flagName}\"" }
 			singleArgs.put(it.flagName, it.default)
+		}
 	}
 	return singleArgs to multipleArgs
 }
