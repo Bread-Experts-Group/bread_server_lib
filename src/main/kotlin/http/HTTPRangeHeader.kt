8069eff634e769f326f876dd1378d796ba823171
ATPStorages <tcgytsc@gmail.com>
2025-04-16 22:47:00 -0500

    Make static

--- src/main/kotlin/http/HTTPRangeHeader.kt
+++ src/main/kotlin/http/HTTPRangeHeader.kt
@@ -6,22 +6,24 @@ class HTTPRangeHeader(
 	val totalSize: Long,
 	val ranges: List<Pair<Long, Long>>
 ) {
-	fun parse(value: String, file: File): HTTPRangeHeader {
-		val (unit, values) = value.split('=')
-		if (unit.lowercase() != "byte") throw UnsupportedOperationException("HTTP Range unit \"$unit\"")
-		var size = 0L
-		var ranges = buildList {
-			values.split(Regex(", ?")).forEach { value ->
-				val (from, to) = value.split(',')
-				val parsed = Pair(
-					if (from.isEmpty()) -1 else from.toLong(),
-					if (to.isEmpty()) file.length() else to.toLong(),
-				)
-				size += if (parsed.first == -1L) file.length() - parsed.second
-				else parsed.second - parsed.first
-				add(parsed)
+	companion object {
+		fun parse(value: String, file: File): HTTPRangeHeader {
+			val (unit, values) = value.split('=')
+			if (unit.lowercase() != "byte") throw UnsupportedOperationException("HTTP Range unit \"$unit\"")
+			var size = 0L
+			var ranges = buildList {
+				values.split(Regex(", ?")).forEach { value ->
+					val (from, to) = value.split(',')
+					val parsed = Pair(
+						if (from.isEmpty()) -1 else from.toLong(),
+						if (to.isEmpty()) file.length() else to.toLong(),
+					)
+					size += if (parsed.first == -1L) file.length() - parsed.second
+					else parsed.second - parsed.first
+					add(parsed)
+				}
 			}
+			return HTTPRangeHeader(size, ranges)
 		}
-		return HTTPRangeHeader(size, ranges)
 	}
 }
