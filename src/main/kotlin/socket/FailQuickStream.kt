53f93c90160f23757674cd3aaddec413c4172567
ATPStorages <tcgytsc@gmail.com>
2025-04-16 21:36:31 -0500

    Risky move

--- src/main/kotlin/socket/FailQuickStream.kt
+++ src/main/kotlin/socket/FailQuickStream.kt
@@ -1,10 +1,17 @@
 package org.bread_experts_group.socket
 
 import java.io.EOFException
+import java.io.IOException
 import java.io.InputStream
 
 class FailQuickStream(private val from: InputStream) : InputStream() {
-	override fun read(): Int = from.read().also {
-		if (it == -1) throw EOFException()
+	override fun read(): Int {
+		val next = try {
+			from.read()
+		} catch (_: IOException) {
+			-1
+		}
+		if (next == -1) throw EOFException()
+		return next
 	}
 }
