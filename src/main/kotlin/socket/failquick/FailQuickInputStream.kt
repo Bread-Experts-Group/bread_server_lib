0a55faad1d4ddd1edc83379bd5de8b80586bc8ea
ATPStorages <tcgytsc@gmail.com>
2025-04-18 19:52:15 -0500

    Fail quick read byte-array override

--- src/main/kotlin/socket/failquick/FailQuickInputStream.kt
+++ src/main/kotlin/socket/failquick/FailQuickInputStream.kt
@@ -3,6 +3,7 @@ package org.bread_experts_group.socket.failquick
 import java.io.EOFException
 import java.io.IOException
 import java.io.InputStream
+import java.util.*
 
 class FailQuickInputStream(private val from: InputStream) : InputStream() {
 	override fun read(): Int {
@@ -16,6 +17,22 @@ class FailQuickInputStream(private val from: InputStream) : InputStream() {
 			throw EOFException()
 		}
 		return next
+	}
+
+	override fun read(b: ByteArray, off: Int, len: Int): Int {
+		Objects.checkFromIndexSize(off, len, b.size)
+		if (len == 0) return 0
+
+		var i = 0
+		try {
+			while (i < len) {
+				b[off + i] = this.read().toByte()
+				i++
+			}
+		} catch (_: EOFException) {
+			if (i == 0) throw EOFException()
+		}
+		return i
 	}
 
 	override fun close() {
