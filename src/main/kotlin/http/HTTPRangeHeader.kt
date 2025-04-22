206b92a591b3b253f1d4a3f87315929b570bcfe4
ATPStorages <tcgytsc@gmail.com>
2025-04-21 19:12:12 -0500

    Range alignment fix

--- src/main/kotlin/http/HTTPRangeHeader.kt
+++ src/main/kotlin/http/HTTPRangeHeader.kt
@@ -15,10 +15,10 @@ class HTTPRangeHeader(
 				values.split(Regex(", ?")).forEach { value ->
 					val (from, to) = value.split('-', ignoreCase = true)
 					var fromL = if (from.isEmpty()) -1L else from.toLong()
-					var toL = if (to.isEmpty()) file.length() else to.toLong()
+					var toL = if (to.isEmpty()) (file.length() - 1) else to.toLong()
 					if (fromL == -1L) {
 						fromL = file.length() - toL
-						toL = file.length()
+						toL = file.length() - 1
 					}
 					size += (toL - fromL) + 1L
 					add(fromL to toL)
