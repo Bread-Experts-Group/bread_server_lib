17a216977dfe67caeed62b8a33cb1297f391d178
ATPStorages <tcgytsc@gmail.com>
2025-05-09 10:10:06 -0500

    <u>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "org.bread_experts_group"
-version = "2.0.1"
+version = "2.0.2"
 
 repositories {
 	mavenCentral()
--- src/main/kotlin/http/html/HTMLUtil.kt
+++ src/main/kotlin/http/html/HTMLUtil.kt
@@ -4,4 +4,4 @@ import org.bread_experts_group.truncateSI
 
 fun truncateSizeHTML(size: Long): String =
 	if (size < 1000) "$size B"
-	else "<span class=\"tooltip\" title=\"$size B\">${truncateSI(size)}B</span>"
+	else "<u title=\"$size B\">${truncateSI(size)}B</span>"
