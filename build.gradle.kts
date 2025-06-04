4564f6ba08ac9a8fd9f19a024174bcb6bf5a3b0b
ATPStorages <tcgytsc@gmail.com>
2025-06-04 16:31:28 -0500

    Spanish resource bundle (from my friends..!)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "org.bread_experts_group"
-version = "2.22.4"
+version = "2.23.0"
 
 repositories {
 	mavenCentral()
--- src/main/kotlin/resource/DirectoryListingResource_en.kt
+++ src/main/kotlin/resource/DirectoryListingResource_en.kt
@@ -2,6 +2,12 @@ package org.bread_experts_group.resource
 
 import java.util.*
 
+/**
+ * @author <a href="https://github.com/ATPStorages">Miko Elbrecht (EN)</a>
+ * Translator
+ * @since 2.19.0
+ */
+@Suppress("unused", "ClassName")
 class DirectoryListingResource_en : ListResourceBundle() {
 	override fun getContents(): Array<out Array<out Any>> = arrayOf(
 		arrayOf("files", "File(s)"),
