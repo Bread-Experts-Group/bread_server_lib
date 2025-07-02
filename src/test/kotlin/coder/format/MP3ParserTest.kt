520fdbfb10e377326fc83e41ef4306f9ef64484a
ATPStorages <tcgytsc@gmail.com>
2025-07-02 00:53:42 -0500

    Higher depth test for MP3 (ID3)

--- src/test/kotlin/coder/format/MP3ParserTest.kt
+++ src/test/kotlin/coder/format/MP3ParserTest.kt
@@ -1,10 +1,13 @@
 package org.bread_experts_group.coder.format
 
+import org.bread_experts_group.coder.format.id3.frame.ID3PictureFrame
 import org.bread_experts_group.coder.format.mp3.MP3Parser
 import org.bread_experts_group.coder.format.mp3.frame.MP3ID3Frame
 import org.bread_experts_group.logging.ColoredHandler
 import org.junit.jupiter.api.Test
 import java.io.InputStream
+import kotlin.io.path.Path
+import kotlin.io.path.writeBytes
 
 class MP3ParserTest {
 	val testFile: InputStream? = this::class.java.classLoader.getResourceAsStream(
@@ -17,7 +20,15 @@ class MP3ParserTest {
 	fun readParsed() {
 		testStream.forEach {
 			when (it) {
-				is MP3ID3Frame -> logger.info(it.id3.toList().toString())
+				is MP3ID3Frame -> {
+					val frames = it.id3.toList()
+					frames.forEach { frame -> logger.info(frame.toString()) }
+					frames.firstNotNullOfOrNull { frame -> frame as? ID3PictureFrame }?.let { frame ->
+						Path("./apic.${frame.mimeType.substringAfter('/')}")
+							.writeBytes(frame.data)
+					}
+				}
+
 				else -> logger.info("$it")
 			}
 		}
