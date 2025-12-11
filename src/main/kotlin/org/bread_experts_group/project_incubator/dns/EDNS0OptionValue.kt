f1a71c78cb05c8caa38a6b2f7291f854d25eaeb0
ATPStorages <tcgytsc@gmail.com>
2025-12-10 21:00:01 -0600

    Move DNS to external project

--- src/main/kotlin/org/bread_experts_group/project_incubator/dns/EDNS0OptionValue.kt
+++
@@ -1,7 +0,0 @@
-package org.bread_experts_group.project_incubator.dns
-
-import org.bread_experts_group.Mappable
-
-enum class EDNS0OptionValue(override val id: UShort, override val tag: String) : Mappable<EDNS0OptionValue, UShort> {
-	COOKIE(10u, "COOKIE")
-}
--- src/main/kotlin/org/bread_experts_group/project_incubator/dns/EDNS0ResponseCode.kt
+++
@@ -1,9 +0,0 @@
-package org.bread_experts_group.project_incubator.dns
-
-import org.bread_experts_group.Mappable
-
-enum class EDNS0ResponseCode(override val id: UInt, override val tag: String) : Mappable<EDNS0ResponseCode, UInt> {
-	;
-
-	override fun toString(): String = stringForm()
-}
